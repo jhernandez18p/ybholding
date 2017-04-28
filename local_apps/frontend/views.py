c80b78655c094c096e6dffc74063d7ce9d5ad440
Josmer Hernández <jhernandez.18p@gmail.com>
2017-04-28 08:30:38 -0500

    Custom errors

--- local_apps/frontend/views.py
+++ local_apps/frontend/views.py
@@ -8,3 +8,47 @@ def home(request):
     pg_title = 'Inicio'
     context['pg_title'] = pg_title
     return render(request, template, context)
+
+
+# Error views
+"""
+	custom errors
+"""
+def my_custom_bad_request_view(request):
+	# template = 'url_error/frontend/400.html'
+    template = 'frontend/index.html'
+    context = {
+		'pg_title':'Error 400',
+		'title':'Error 400',
+	}
+    return render(request, template, context)
+
+
+def my_custom_permission_denied_view(request):
+	# template = 'url_error/frontend/403.html'
+    template = 'frontend/index.html'
+    context = {
+		'pg_title':'Error 403',
+		'title':'Error 403',
+    }
+    return render(request, template, context)
+
+
+def my_custom_page_not_found_view(request):
+	# template = 'url_error/frontend/404.html'
+    template = 'frontend/index.html'
+    context = {
+		'pg_title':'Error 404',
+		'title':'Error 404',
+    }
+    return render(request, template, context)
+
+
+def my_custom_error_view(request):
+	# template = 'url_error/frontend/500.html'
+    template = 'frontend/index.html'
+    context = {
+		'pg_title':'Error 500',
+		'title':'Error 500',
+    }
+    return render(request, template, context)
