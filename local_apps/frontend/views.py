1a51783e8968fe0adf20c5fa9f7554daa8000da7
Josmer Hernández <jhernandez.18p@gmail.com>
2017-04-28 09:12:58 -0500

    FIxes

--- local_apps/frontend/views.py
+++ local_apps/frontend/views.py
@@ -15,40 +15,44 @@ def home(request):
 	custom errors
 """
 def my_custom_bad_request_view(request):
+    """# Custom http 400 error"""
 	# template = 'url_error/frontend/400.html'
     template = 'frontend/index.html'
     context = {
-		'pg_title':'Error 400',
-		'title':'Error 400',
-	}
+        'pg_title':'Error 400',
+        'title':'Error 400',
+    }
     return render(request, template, context)
 
 
 def my_custom_permission_denied_view(request):
+    """# Custom http 403 error"""
 	# template = 'url_error/frontend/403.html'
     template = 'frontend/index.html'
     context = {
-		'pg_title':'Error 403',
-		'title':'Error 403',
+        'pg_title':'Error 403',
+        'title':'Error 403',
     }
     return render(request, template, context)
 
 
 def my_custom_page_not_found_view(request):
+    """# Custom http 404 error"""
 	# template = 'url_error/frontend/404.html'
     template = 'frontend/index.html'
     context = {
-		'pg_title':'Error 404',
-		'title':'Error 404',
+        'pg_title':'Error 404',
+        'title':'Error 404',
     }
     return render(request, template, context)
 
 
 def my_custom_error_view(request):
+    """# Custom http 500 error"""
 	# template = 'url_error/frontend/500.html'
     template = 'frontend/index.html'
     context = {
-		'pg_title':'Error 500',
-		'title':'Error 500',
+        'pg_title':'Error 500',
+        'title':'Error 500',
     }
     return render(request, template, context)
